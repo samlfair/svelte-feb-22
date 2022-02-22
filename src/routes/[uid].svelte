e7a1f145c0e708229775adaf7adb2df6ac99978d
Sam Littlefair <sam.littlefair@prismic.io>
2022-02-22 15:30:37 +0000

    Init

---
+++ src/routes/[uid].svelte
@@ -0,0 +1,35 @@
+<!-- ~/src/routes/index.svelte -->
+
+<script>
+  import * as prismicH from '@prismicio/helpers';
+  
+  export let document;
+</script>
+
+<main>
+  <div class="header container" style="background-image: url('{document.data.image.url}')">	 
+    <h1>
+      {document.data.title}
+    </h1>
+  </div>
+  <div class="container">
+    <div class="text">
+      {@html prismicH.asHTML(document.data.content)}
+    </div>
+  </div>
+</main>
+
+<style>
+  .container > * {
+    width: 100%;
+    max-width: 700px;
+  }
+
+  .header {
+    color: white;
+    background-size: cover;
+    min-height: 25vw;
+    padding-top: 2rem;
+    justify-content: flex-end;
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,34 @@
+<!-- ~/src/routes/__layout.svelte -->
+
+<script>
+  import Nav from "./../lib/nav.svelte"
+  import "./../styles/reset.css";
+  import "./../styles/globals.css"
+</script>
+
+<div class="flex-layout">
+  <header class="absolute">
+    <Nav />
+  </header>
+  <slot></slot>
+  <p class="footer">© Acme Corp, 1951</p>
+</div>
+
+<style>
+  .absolute {
+    position: absolute;
+    width: 100%;
+  }
+
+  .footer {
+    margin: 3rem 0;
+    text-align: center;
+  }
+
+  .flex-layout {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+</style>
